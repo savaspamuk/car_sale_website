689431c5cc47fbb53502a12d2108d95ce1034937
Savas Pamuk <savaspamuk@gmail.com>
2024-01-09 15:44:27 +0100

    feat: build header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header__top">
+      <Container>
+        <Col lg="6" md="6" sm="6">
+          <div className="header__top__left">
+            <span>Need Help?</span>
+            <span className="header__top__help">
+              <i class="ri-phone-fill"></i>+46-123-456-78-90
+            </span>
+          </div>
+        </Col>
+
+        <Col lg="6" md="6" sm="6">
+          <div className="header__top__right">
+            <Link to="/#">
+              <i class="ri-login-circle-line"></i> Login
+            </Link>
+            <Link to="/#">
+              <i class="ri-user-line"></i> Register
+            </Link>
+          </div>
+        </Col>
+      </Container>
+    </div>
+  );
 };
 
 export default Header;
