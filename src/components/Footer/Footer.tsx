70477721f286a7c9e141bf8bd102971cf7a7dcbc
Savas Pamuk <savaspamuk@gmail.com>
2024-03-01 10:52:46 +0100

    test Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -53,23 +53,25 @@ const Footer: React.FC = () => {
           <Col lg="3" md="4" sm="6">
             <div className="mb-4">
               <h5 className="footer__link-title mb-4">Head Office</h5>
-              <p className="office__info">
+              <p className="office__info" data-testid="office-info">
                 <a href="https://www.google.com/maps/place/411+06+Nordstaden/@57.7069787,11.9276906,13z/data=!3m1!4b1!4m6!3m5!1s0x464ff36444097555:0xf5b54137131e1636!8m2!3d57.70694!4d11.96889!16s%2Fg%2F11rbh4kvsp?entry=ttu">
                   <i className="ri-map-pin-2-fill"></i> 123, Göteborg, Sweden
                 </a>
               </p>
-              <p className="office__info">
+              <p className="office__info" data-testid="office-info">
                 <a href="tel: +46 123 456 78 90">
                   <i className="ri-phone-fill"></i> +46 123 456 78 90
                 </a>
               </p>
-              <p className="office__info">
+              <p className="office__info" data-testid="office-info">
                 <a href="mailto: abc@abc.com">
                   <i className="ri-mail-fill"></i> abc@abc.com
                 </a>
               </p>
-              <p className="office__info">Follow Us</p>
-              <p className="office__info">
+              <p className="office__info" data-testid="office-info">
+                Follow Us
+              </p>
+              <p className="office__info" data-testid="office-info">
                 <a href="#" className="social__link-icon instagram">
                   <i className="ri-instagram-line"></i>
                 </a>
